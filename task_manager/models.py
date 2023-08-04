f9635fafa9a054bd9f598ca39f6619a92a8aed98
Nataliia Podoroha <ingenuous999@gmail.com>
2023-08-04 16:40:48 +0200

    Update Task model in models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -72,7 +72,7 @@ class Task(models.Model):
     )
 
     class Meta:
-        ordering = ["priority"]
+        ordering = ["is_completed"]
 
     def __str__(self):
         return f"{self.name} (Priority: {self.priority})"
