e4a962955022badb9490a456173eec38b6e3896e
Nataliia Podoroha <ingenuous999@gmail.com>
2023-08-09 11:02:24 +0200

    Add SECRET_KEY to .env

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#t(7)+83dio7bo5=zr)8j%s1jqarmulw3f54#$+3^ut)@)1zgg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
